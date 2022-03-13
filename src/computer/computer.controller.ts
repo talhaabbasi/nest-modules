a7b6b6afa69b829fcde1ccb1bd34c7d2e3dc1271
talhaabbasi <talhaabbasi1997@gmail.com>
2022-03-13 22:06:36 +0500

    [FEAT] Update Computer controller

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from '../cpu/cpu.service';
+import { DiskService } from '../disk/disk.service';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController {
+  constructor(
+    private readonly cpu: CpuService,
+    private readonly disk: DiskService,
+  ) {}
+
+  @Get()
+  run() {
+    return [this.cpu.compute(1, 2), this.disk.getData()];
+  }
+}
--- src/computer/computer.module.ts
+++ src/computer/computer.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CpuModule } from '../cpu/cpu.module';
+import { DiskModule } from '../disk/disk.module';
+import { ComputerController } from './computer.controller';
 
-@Module({})
+@Module({
+  imports: [DiskModule, CpuModule],
+  controllers: [ComputerController],
+})
 export class ComputerModule {}
