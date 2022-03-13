65987278f7b3af112357f7438a4581950b427f88
talhaabbasi <talhaabbasi1997@gmail.com>
2022-03-13 20:26:22 +0500

    [FIX] Fix dependencies

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PowerService } from '../power/power.service';
+import { PowerModule } from '../power/power.module';
 import { DiskService } from './disk.service';
 
 @Module({
-  imports: [PowerService],
+  imports: [PowerModule],
   providers: [DiskService],
   exports: [DiskService],
 })
