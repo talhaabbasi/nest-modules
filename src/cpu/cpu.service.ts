896e5ba1f75a6d311257bd1f8ad3a6f05342e746
talhaabbasi <talhaabbasi1997@gmail.com>
2022-03-13 20:01:04 +0500

    [FEAT] Add compute method

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from '../power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private readonly power: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from PowerService');
+    this.power.supplyPower(10);
+    return a + b;
+  }
 }
